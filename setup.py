a993157c16ec648ce39c9fa02e340e134ea596c9
Umesh94kr <umeshkumar94628@gmail.com>
2025-10-19 14:08:02 +0530

    Added logging and Exception

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Umesh Kumar",
+    author_email="umeshkumar94628@gmail.com",
+    packages=find_packages()
+)
